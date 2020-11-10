b0e690b2fc017e1849243d29c26fc62b6f3a4e8f
Kelvin <tobar411@gmail.com>
2020-11-10 03:43:59 +0000

    added another template with form, which user submit value to longURL

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,9 +12,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  const result = Math.random().toString(36).substring(2,8)
+  return result;
+}
+
 app.get("/urls", (req,res)=> {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+})
+
+app.post("/urls", (req, res)=> {
+  console.log(req.body);
+  res.send("Ok");
+})
+
+app.get("/urls/new", (req,res)=> {
+  res.render("urls_new");
 })
 
 app.get("/urls/:shortURL", (req,res)=> {
